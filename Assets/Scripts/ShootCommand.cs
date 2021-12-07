7eb4a9e384d66e1da080dbd0fd60841e1fa7820c
Firmen-net <maduragame1810@gmail.com>
2021-12-07 22:25:58 +0700

    main

---
+++ Assets/Scripts/ShootCommand.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootCommand : Command
+{
+    PlayerShooting playerShooting;
+
+    public ShootCommand(PlayerShooting _playerShooting)
+    {
+        playerShooting = _playerShooting;
+    }
+
+    public override void Execute()
+    {
+        //Player menembak
+        playerShooting.Shoot();
+    }
+
+    public override void UnExecute()
+    {
+    }
+}
